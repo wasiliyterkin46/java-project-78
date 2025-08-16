79e56a96f88436a03a1a1fe12aec26a108497e75
wasiliyterkin46 <wasiliy_terkin@mail.ru>
2025-08-16 22:37:52 +0300

    Step 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/wasiliyterkin46/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/wasiliyterkin46/java-project-78/actions)
+### sonarqube status
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=wasiliyterkin46_java-project-78&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=wasiliyterkin46_java-project-78)
+
---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+    jacoco
+    id("org.sonarqube") version "6.2.0.5505"
+    id("io.freefair.lombok") version "8.13.1"
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+
+    testImplementation(platform("org.junit:junit-bom:5.12.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.2")
+
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+}
+
+/*application {
+    mainClass = "hexlet.code.App"
+}*/
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+checkstyle {
+    toolVersion = "10.21.4"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sonar {
+    properties {
+        properties {
+            property("sonar.projectKey", "wasiliyterkin46_java-project-78")
+            property("sonar.organization", "wasiliyterkin46")
+        }
+}
